4e14a5d1e93e93d31fa5093660c1e33585cad557
Rohit Verma <rohit.verma@niki.ai>
2019-06-21 14:55:30 +0530

    upgraded-tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "source_documents" {
-  type        = "list"
+  type        = list(string)
   description = "List of JSON IAM policy documents.<br/><br/><b>Limits:</b><br/>* List size max 10<br/> * Statement can be overriden by the statement with the same sid from the latest policy."
   default     = []
 }
+
